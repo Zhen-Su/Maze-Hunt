7b0ee196617bb448dbf63f6577da3ca7ee7aed08
James <jlb838@student.bham.ac.uk>
2020-01-21 21:48:55 +0000

    Adjusted the file strucutre

---
+++ src/com/project/AIPlayer.java
@@ -0,0 +1,17 @@
+package com.project;
+import java.lang.Math;
+public class AIPlayer extends Player {
+
+  public AIPlayer() {
+    super("AI");
+    super.coins = 5;
+  }
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+  public int direction(int numberofExits) {
+    return (int)(Math.random() * ((numberofExits - 1) + 1)) + 1;
+
+  }
+}
---
+++ src/com/project/test.java
@@ -0,0 +1,26 @@
+package com.project;
+public class test {
+  public static void main(String[] args) {
+    Player player1 = new Player("James");
+    System.out.println(player1.name);
+    System.out.println(player1.coins);
+    System.out.println(player1.health);
+    player1.decreaseHealth();
+    System.out.println(player1.health);
+    player1.decreaseHealth(2);
+    player1.generateHealth();
+    System.out.println(player1.health);
+    player1.pickUpCoins(5);
+    System.out.println(player1.coins);
+    player1.pickUpItem("Sword");
+    player1.pickUpItem("Compass");
+    System.out.println(player1.toString());
+    player1.death();
+    System.out.println(player1.toString());
+    AIPlayer All = new AIPlayer();
+    System.out.println(All.name);
+    System.out.println(All.coins);
+    System.out.println(All.toString());
+    System.out.println(All.direction(4));
+  }
+}
