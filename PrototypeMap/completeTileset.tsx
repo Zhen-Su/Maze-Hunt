9fb5662a10fe14785467fd85574751c27e2a9da8
Charlotte <cxb980@student.bham.ac.uk>
2020-01-28 12:11:48 +0000

    Updated map and tileset
    changed file locations

--- PrototypeMap/completeTileset.tsx
+++ PrototypeMap/completeTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="2tiles" tilewidth="100" tileheight="100" tilecount="5" columns="5">
- <image source="../sending/2tiletileset.png" width="500" height="100"/>
+ <image source="CompleteTileset.png" width="500" height="100"/>
 </tileset>
