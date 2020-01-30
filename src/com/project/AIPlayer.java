75de74ca6e13061bb9e63e4a41c3b2e60a2af208
James <jlb838@student.bham.ac.uk>
2020-01-30 21:50:05 +0000

    Added some stuff waiting to integrate collectible code but aim is to do that over the weekend

--- src/com/project/AIPlayer.java
+++ src/com/project/AIPlayer.java
@@ -6,26 +6,27 @@ public class AIPlayer extends Player {
 
   public AIPlayer() {
     super("AI");
+
     super.coins = 5;
+
     int randomx = (int)(Math.random() * ((100 - 0) +1));
     int randomy = (int)(Math.random() * ((100 - 0) +1));
     super.position = new Pair(randomx, randomy);
   }
+
   @Override
   public String toString() {
     return super.toString();
   }
+
   public Pair direction(ArrayList<Integer> openDoor) {
     int randomTake = (int)(Math.random() * ((openDoor.size() - 1) + 1));
     return openDoor.get(randomTake);
   }
-  @Override
-  public void death() {
-    //some command to remove player
-    System.out.println("AI Player is dead");
-  }
+
   // will need method to gather all squares which are available and put them in a list
   public void play() {
+
     while(true /* will be replaced by timmer boolean */) {
       ArrayList<Integer> test = new ArrayList<>();
       test.add(1);
@@ -40,4 +41,5 @@ public class AIPlayer extends Player {
       }
     }
   }
+  
 }
--- src/com/project/Player.java
+++ src/com/project/Player.java
@@ -3,62 +3,77 @@ import java.util.ArrayList;
 // import java.awt.KeyEvent;
 // import java.awt.geom.Point2D;
 public class Player {
-  public int health;
-  public int coins;
-  public String name;
-  public ArrayList<String> items;
-  public Pair position;
-  public boolean hasSword;
-  public boolean hasHealingPotion;
-  public boolean hasShield;
   public boolean hasCoin;
   public boolean hasCompass;
   public boolean hasDamagingPotion;
-  public int swordDamage = 0;
+  public boolean hasHealingPotion;
+  public boolean hasShield;
+  public boolean hasSword;
 
-  // Point position;
+  public int coins;
+  public int health;
+  public int ID;
+  public int swordDamage;
 
-  public Player(String name) {
+  public String name;
+  public ArrayList<String> items;
+  public Pair position;
+
+
+  public Player(String name, int ID;) {
     this.health = 5;
     this.coins = 0;
     this.name = name;
     this.items = new ArrayList<>();
     // this.position = new Point(0, 0);
     this.position = new Pair(1, 2);
+    this.swordDamage = 0;
+    this.ID = ID;
   }
-  public void decreaseHealth() {
+  public int getID() {
+    return this.ID;
+  }
 
+  public void decreaseHealth() {
     this.health--;
+
     if (this.health == 0) {
       this.death();
     }
   }
+
   public void generateHealth() {
     if(health != 5) {
     this.health++;
     }
   }
+
   public void pickUpCoins(int coinPick) {
     this.coins += coinPick;
   }
+
   public void death() {
     System.out.println("Player has died respawning now");
     this.health = 5;
     this.coins = 0;
     this.items = new ArrayList<>();
   }
+
   public void pickUpItem(String item) {
     this.items.add(item);
   }
+
   public void decreaseHealth(int number) {
     this.health -= number;
     if(health <= 0) {
       this.death();
     }
   }
+
   public String toString() {
     return "Name: " + this.name + " Health: " + this.health + " Coins: " + this.coins + " Items " + this.items + " Postion: " + position.toString();
   }
+
   public void playerHitPlayer(Player hit) {
     // write boolean to check sword
 
@@ -71,20 +86,22 @@ public class Player {
         this.swordDamage++;
         this.coins += hit.coins;
         hit.death();
-
       }
     }
     //need to add shield stuffr
   }
+
   public void playerKillAI(AIPlayer AI) {
     if (AI.health == 0) {
     this.pickUpCoins(5);
   } else {
     AI.decreaseHealth();
   }
+
   public void move(Pair coord) {
     this.position = coord;
   }
+
   public void changeXAndY(int x, int y) {
     this.poistion.getX() += x;
     this.position.getY() += y;
