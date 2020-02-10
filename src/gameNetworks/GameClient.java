12b718caf95f025fcf5bf6a2432018462c1ab3b0
Yueyi Wang <yxw835@cs.bham.ac.uk>
2020-02-10 23:05:19 +0000

    use java swing to demo

--- src/gameNetworks/GameClient.java
+++ src/gameNetworks/GameClient.java
@@ -1,39 +1,193 @@
 package gameNetworks;
 
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import player.Player;
 
 /**
  * 
  * @author kevin
  *
  */
-public class GameClient {
+public class GameClient extends Frame {
 
+	public static final int FRAME_WIDTH = 800;
+	public static final int FRAME_HEIGHT = 600;
+	public static final String GAME_NAME = "--GAMENAME--";
 	private NetClient nc = new NetClient(this);
+	private Player player;
+	private List<Player> players = new ArrayList<>();
+	private Image offScreenImage = null;
+	private ConDialog dialog = new ConDialog();
 
-	
-	public void startClient() {
-		
-		//In MVP, we need to enter Server IP manually
-		Scanner sc = new Scanner(System.in); 
-		System.out.println("Please enter Server IP: ");
-		String serverIP = sc.nextLine();
-		System.out.println("Please enter your UDP Port (Any Number): ");
-		int clientUDPPort = sc.nextInt();
-				
-		nc.setClientUDPPort(clientUDPPort);
-		nc.connect(serverIP,GameServer.SERVER_TCP_PORT);
-		
-				
-		sc.close();
-		//public IP: 8.208.8.136
-		//private ip:10.222.143.1
+	@Override
+	public void paint(Graphics g) {
+		for (int i = 0; i < players.size(); i++) {
+			Player t = players.get(i);
+			t.draw(g);
+		}
+		player.draw(g);
 	}
-	
 
-	
+	public NetClient getNc() {
+		return nc;
+	}
+
+	public void setNc(NetClient nc) {
+		this.nc = nc;
+	}
+
+	//this method to fix screen	flicker problem
+	@Override
+	public void update(Graphics g) {
+		if (offScreenImage == null) {
+			offScreenImage = this.createImage(800, 600);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
+
+//	public void startClient() {
+//
+//		// In MVP, we need to enter Server IP manually
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Please enter Server IP: ");
+//		String serverIP = sc.nextLine();
+//		System.out.println("Please enter your UDP Port (Any Number): ");
+//		int clientUDPPort = sc.nextInt();
+//
+//		nc.setClientUDPPort(clientUDPPort);
+//		nc.connect(serverIP, GameServer.SERVER_TCP_PORT);
+//
+//		sc.close();
+//	}
+
 	public static void main(String[] args) {
-		new GameClient().startClient();
+		GameClient gc = new GameClient();
+//		gc.startClient();
+		gc.launch();
 	}
-	
+
+	private void launch() {
+
+		this.setLocation(400, 300);
+		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		this.setTitle(GAME_NAME);
+
+		// This is a windows events, the purpose is to exit normally when we close the
+		// window
+		this.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
+		this.setResizable(false);
+		this.setBackground(Color.BLACK);
+		this.addKeyListener(new KeyMonitor());
+		this.setVisible(true);
+		
+		new Thread(new PaintThread()).start();
+
+		dialog.setVisible(true);
+	}
+
+	/**
+	 * paint thread class
+	 * 
+	 * @author kevin
+	 *
+	 */
+	class PaintThread implements Runnable {
+
+		public void run() {
+			while (true) {
+				repaint();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * we can make this inner class as an outer class in gameEvents package
+	 * 
+	 * @author kevin
+	 *
+	 */
+	class KeyMonitor extends KeyAdapter {
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			player.keyReleased(e);
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			player.keyPressed(e);
+		}
+	}
+
+    class ConDialog extends Dialog{
+        Button b = new Button("connect to server");
+        TextField tfIP = new TextField("127.0.0.1", 15);
+        TextField tfMyUDPPort = new TextField("5555", 4);
+
+        public ConDialog() {
+            super(GameClient.this, true);
+            this.setLayout(new FlowLayout());
+            this.add(new Label("IP:"));
+            this.add(tfIP);
+            this.add(new Label("My UDP Port:"));
+            this.add(tfMyUDPPort);
+            this.add(b);
+            this.setLocation(400, 400);
+            this.pack();
+            this.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    setVisible(false);
+                }
+            });
+            b.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String IP = tfIP.getText().trim();
+                    int myUDPPort = Integer.parseInt(tfMyUDPPort.getText().trim());
+                    nc.setClientUDPPort(myUDPPort);
+                    nc.connect(IP, GameServer.SERVER_TCP_PORT);
+                    setVisible(false);
+                }
+            });
+        }
+    }
 }
+
+
---
+++ src/player/Player.java
@@ -0,0 +1,197 @@
+package player;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import gameNetworks.GameClient;
+import gameNetworks.NewMessage;
+
+public class Player {
+
+	public int id;
+	private GameClient gcClient;
+
+	public static final int X_SPEED = 5;
+	public static final int Y_SPEED = 5;
+
+	private int x;
+	private int y;
+
+	private GameClient gameClient;
+
+	private boolean bL, bU, bR, bD;
+	private Dir dir = Dir.STOP;
+
+	public enum Dir {
+		L, LU, U, RU, R, RD, D, LD, STOP
+	}
+
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Player(int x, int y, GameClient gameClient) {
+		this(x, y);
+		this.gameClient = gameClient;
+	}
+
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_A:
+			bL = true;
+			break;
+		case KeyEvent.VK_W:
+			bU = true;
+			break;
+		case KeyEvent.VK_D:
+			bR = true;
+			break;
+		case KeyEvent.VK_S:
+			bD = true;
+			break;
+		}
+		locateDirection();
+	}
+
+	private void locateDirection() {
+		// TODO Auto-generated method stub
+		Dir oldDir = this.dir;
+		if (bL && !bU && !bR && !bD)
+			dir = Dir.L;
+		else if (bL && bU && !bR && !bD)
+			dir = Dir.LU;
+		else if (!bL && bU && !bR && !bD)
+			dir = Dir.U;
+		else if (!bL && bU && bR && !bD)
+			dir = Dir.RU;
+		else if (!bL && !bU && bR && !bD)
+			dir = Dir.R;
+		else if (!bL && !bU && bR && bD)
+			dir = Dir.RD;
+		else if (!bL && !bU && !bR && bD)
+			dir = Dir.D;
+		else if (bL && !bU && !bR && bD)
+			dir = Dir.LD;
+		else if (!bL && !bU && !bR && !bD)
+			dir = Dir.STOP;
+
+		if (dir != oldDir) {
+
+			/**
+			 * If the new direction is different from old direction, client will send a
+			 * message to server, then server will broadcast this messsage to all clients.
+			 **/
+
+			// NewMessage msg = new NewMessage(id, x, y, dir);
+			gcClient.getNc().send(msg);
+		}
+
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_A:
+			bL = false;
+			break;
+		case KeyEvent.VK_W:
+			bU = false;
+			break;
+		case KeyEvent.VK_D:
+			bR = false;
+			break;
+		case KeyEvent.VK_S:
+			bD = false;
+			break;
+		}
+		locateDirection();
+	}
+
+	public void draw(Graphics g) {
+
+		Color c = g.getColor();
+		g.setColor(Color.BLUE);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);
+
+		switch (dir) {
+		case L:
+			x -= X_SPEED;
+			break;
+		case LU:
+			x -= X_SPEED;
+			y -= Y_SPEED;
+			break;
+		case U:
+			y -= Y_SPEED;
+			break;
+		case RU:
+			x += X_SPEED;
+			y -= Y_SPEED;
+			break;
+		case R:
+			x += X_SPEED;
+			break;
+		case RD:
+			x += X_SPEED;
+			y += Y_SPEED;
+			break;
+		case D:
+			y += Y_SPEED;
+			break;
+		case LD:
+			x -= X_SPEED;
+			y += Y_SPEED;
+			break;
+		case STOP:
+			break;
+		}
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public GameClient getGameClient() {
+		return gameClient;
+	}
+
+	public void setGameClient(GameClient gameClient) {
+		this.gameClient = gameClient;
+	}
+
+	public Dir getDir() {
+		return dir;
+	}
+
+	public void setDir(Dir dir) {
+		this.dir = dir;
+	}
+
+}
