5f092a1b0cf352753d250737f9185dd53e022088
aaf805 <aaf805@cs.bham.ac.uk>
2020-03-02 22:09:20 +0000

    Added documentation for the Timer class

--- core/src/com/project/mazegame/tools/Timer.java
+++ core/src/com/project/mazegame/tools/Timer.java
@@ -1,13 +1,25 @@
 package com.project.mazegame.tools;
 
+/**
+*Code written by: Andin
+ **/
 public class Timer {
     private float counter;
     private int time;
 
+    /**
+     * Call constructer to initialize the object
+     * The constructor will initialize the time to zero
+     */
     public Timer() {
         time = 0;
     }
 
+    /**
+     * must be called inside the render method
+     * @param delta to keep track of time
+     *             To check time, call currentTime();
+     */
     public void updateTimer(float delta) {
         counter += delta;
         if (counter >= 1) {
@@ -16,6 +28,11 @@ public class Timer {
         }
     }
 
+    /**
+     * Will return the current time from when the object is initialized, given that the updateTimer(float delta) method is called inside the render method.
+     * The time return is in seconds.
+     * @return
+     */
     public int currentTime() {
         return (int) time;
     }
