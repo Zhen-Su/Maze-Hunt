d3f26c11acb85f99e5a8c5c57cdcc03d6f076ead
aaf805 <aaf805@cs.bham.ac.uk>
2020-03-05 11:46:44 +0000

    Added sample test case class

---
+++ core/src/com/project/mazegame/Testing/TestCasesSinglePlayer.java
@@ -0,0 +1,4 @@
+package com.project.mazegame.Testing;
+
+public class TestCasesSinglePlayer {
+}
---
+++ core/src/com/project/mazegame/Testing/TestMultiPlayer.java
@@ -0,0 +1,13 @@
+package com.project.mazegame.Testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMultiPlayer {
+    @Test
+    public void getPlayerCoordingate() {
+
+//        assertEquals();
+    }
+}
