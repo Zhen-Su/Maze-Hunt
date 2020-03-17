1944b036676b69f40435b7e8270c529fac898b2b
aaf805 <aaf805@cs.bham.ac.uk>
2020-03-17 13:20:24 +0000

    Added more testing classes with dedicated folder

---
+++ core/src/test/java/com/project/mazegame/testing/PlayerTest.java
@@ -0,0 +1,64 @@
+package com.project.mazegame.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getswordXP() {
+    }
+
+    @Test
+    public void getShieldXP() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void decreaseHealth() {
+    }
+
+    @Test
+    public void attack() {
+    }
+
+    @Test
+    public void generateHealth() {
+    }
+
+    @Test
+    public void getPosition() {
+    }
+
+    @Test
+    public void nearestPlayer() {
+    }
+
+    @Test
+    public void isplayerSameSpace() {
+    }
+
+    @Test
+    public void playerOnSameSpace() {
+    }
+
+    @Test
+    public void hasSword() {
+    }
+
+    @Test
+    public void hasShield() {
+    }
+
+    @Test
+    public void getHealth() {
+    }
+
+    @Test
+    public void getID() {
+    }
+}
